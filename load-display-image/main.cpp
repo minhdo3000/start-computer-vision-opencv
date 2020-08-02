bdb36bc7366d737935492abd09c83f1dd8afa902
tonydo1000 <dovanminh1000@gmail.com>
2020-08-02 11:19:50 +0800

    load and display image

---
+++ load-display-image/main.cpp
@@ -0,0 +1,30 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+    Mat image = imread("../test.png"); // Read the image file
+
+    if (image.empty()) // Check for failure
+    {
+        cout << "Could not open or find the image" << endl;
+        cin.get(); //wait for any key press
+        return -1;
+    }
+
+    String windowName = "Test"; //Name of the window
+
+    namedWindow(windowName); // Create a window
+
+    imshow(windowName, image); // Show our image inside the created window.
+
+    waitKey(0); // Wait for any keystroke in the window
+
+    destroyWindow(windowName); //destroy the created window
+
+    return 0;
+}
